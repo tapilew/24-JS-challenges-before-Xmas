741256e3ebe64d79eb1f1cfbc8994d7520169610
Luis Tapia Navarro <86934099+tapilew@users.noreply.github.com>
2023-12-14 06:43:16 -0500

    feat: solve Platzi JS day 01

---
+++ js/01/README.md
@@ -0,0 +1,4 @@
+# Day 01
+
+- variables, functions, basic syntax
+- datatypes
---
+++ js/01/main.js
@@ -0,0 +1,5 @@
+function getTypeOf(value) {
+  return typeof value;
+}
+
+console.log(getTypeOf(1), getTypeOf("Word"), getTypeOf(true));
